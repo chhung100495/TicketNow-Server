dbf9b4717093bed7f9179addaa958367df0a093a
Châu Hải Hùng <haihung100495@gmail.com>
2019-03-27 09:07:47 +0700

    change process.env.port => change process.env.PORT

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 var host = '0.0.0.0';
-var port = process.env.port || 4000;
+var port = process.env.PORT || 4000;
 app.listen(port, host, () => {
     console.log(`API running on port ${port}`);
 })
