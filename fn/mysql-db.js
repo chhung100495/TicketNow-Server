736a267b57392f8880140203ed1e816f827c4ddd
Châu Hải Hùng <haihung100495@gmail.com>
2019-03-27 08:45:31 +0700

    config db for deloy.

--- fn/mysql-db.js
+++ fn/mysql-db.js
@@ -1,11 +1,20 @@
 var mysql = require('mysql');
 
+// var config = {
+//     host     : 'localhost',
+//     port     : '3306',
+//     user     : 'root',
+//     password : '',
+//     database : 'booking_tickets'
+// };
+
+// mysql://b6a2871d0e51fc:cd5fd474@us-cdbr-iron-east-03.cleardb.net/heroku_9795ac331e55444?reconnect=true
 var config = {
-    host     : 'localhost',
+    host     : 'us-cdbr-iron-east-03.cleardb.net',
     port     : '3306',
-    user     : 'root',
-    password : '',
-    database : 'booking_tickets'
+    user     : 'b6a2871d0e51fc',
+    password : 'cd5fd474',
+    database : 'heroku_9795ac331e55444'
 };
 
 exports.load = function(sql) {
