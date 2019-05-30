d95b7faae3967efb017000578e1a13370014bdef
chhung100495 <haihung100495@gmail.com>
2019-05-30 09:26:01 +0700

    change the number of days to receive notifications

--- repositories/notificationsRepo.js
+++ repositories/notificationsRepo.js
@@ -3,7 +3,7 @@ var db = require('../fn/mysql-db');
 exports.loadAll = function(id) {
     var sql = `SELECT n.id, n.img_url as imgURL, n.title, n.brief, n.content, n.type, n.created_at as createdAt
         FROM notifications as n
-        WHERE DATEDIFF(CURDATE(), n.created_at) <= 27
+        WHERE DATEDIFF(CURDATE(), n.created_at) <= 7
         ORDER BY n.created_at DESC`;
     return db.load(sql);
 }
