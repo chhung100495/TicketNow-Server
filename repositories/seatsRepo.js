bccead60cca63c27e2947046a754ac74535072ff
chhung100495 <haihung100495@gmail.com>
2019-06-12 21:43:45 +0700

    change value status of seats

--- repositories/seatsRepo.js
+++ repositories/seatsRepo.js
@@ -1,7 +1,7 @@
 var db = require('../fn/mysql-db');
 
 exports.loadByShowTime = (movieShowingsID, locationID) => {
-    var sql = `SELECT s.id, s.row, s.number, s.type, s.block_id as blockID, IF(bk.id IS NULL, 0, 1) as status
+    var sql = `SELECT s.id, s.row, s.number, s.type, s.block_id as blockID, IF(bk.id IS NULL, 1, 0) as status
         FROM seats as s
         LEFT JOIN booked_seats as bs ON bs.seat_id = s.id
         LEFT JOIN bookings as bk ON bk.id = bs.booking_id AND bk.movie_showings_id = '${movieShowingsID}'
@@ -10,7 +10,7 @@ exports.loadByShowTime = (movieShowingsID, locationID) => {
 }
 
 exports.loadBySale = (saleID, locationID) => {
-    var sql = `SELECT s.id, s.row, s.number, s.type, s.block_id as blockID, IF(bk.id IS NULL, 0, 1) as status
+    var sql = `SELECT s.id, s.row, s.number, s.type, s.block_id as blockID, IF(bk.id IS NULL, 1, 0) as status
         FROM seats as s
         LEFT JOIN booked_seats as bs ON bs.seat_id = s.id
         LEFT JOIN bookings as bk ON bk.id = bs.booking_id AND bk.sale_id = '${saleID}'
